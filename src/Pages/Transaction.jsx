571509641fba167648c51514f225067c712f5e20
falak50 <83400991+falak50@users.noreply.github.com>
2024-03-08 13:18:29 -1200

    done for the final time

--- src/Pages/Transaction.jsx
+++ src/Pages/Transaction.jsx
@@ -19,14 +19,16 @@ const Transaction = () => {
 
     return (
         <div className="m-10">
-           <div className="bg-[#deabab] items-center w-[40%] m-auto px-10 py-10 ">
-            <h1 className="text-xl">Transfer Money</h1>
-            <h1>Name :  {user?.name} </h1>
-            <h1>Total : {user?.AmountBDT} BDT </h1> 
-            <p>Acount Number : {id} </p>
-           </div>
+         
            
            {/* <Transaction></Transaction> */}
+           <div className="bg-[#deabab] items-center w-[40%] m-auto px-10 py-10 ">
+            <h1 className="text-xl my-2 font-semibold">Withdraw Money</h1>
+            <h1>Name :  {user?.name} </h1>
+            <p>Acount Number : {id} </p>
+            <p>Email : {user.email}</p>
+            <p>Phone : {user.phone}</p>
+           </div>
 
             <TransectionForm 
             sendMoneyID={id}
